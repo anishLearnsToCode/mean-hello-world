dc81b705c81d0c2780bfc8502f9eb0c9bcb26949
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2018-06-12 12:51:59 +0530

    imported MaExpansionModule in app.module.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule } from '@angular/material';
+import { MatInputModule, MatCardModule, MatButtonModule, MatToolbarModule, MatExpansionModule } from '@angular/material';
 import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PostEditComponent } from './Posts/post-edit/post-edit.component';
 import { HeaderComponent } from './header/header.component';
 import { PostsListComponent } from './Posts/posts-list/posts-list.component';
+import {MatExpansionModule} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { PostsListComponent } from './Posts/posts-list/posts-list.component';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
