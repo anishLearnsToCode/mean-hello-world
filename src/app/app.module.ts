d75b05f285647976c86b187c0bae7575e69b9499
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2018-06-12 11:55:20 +0530

    added Posts Edit Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostEditComponent } from './Posts/post-edit/post-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostEditComponent
   ],
   imports: [
     BrowserModule
