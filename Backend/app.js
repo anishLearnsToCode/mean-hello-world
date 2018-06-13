6d6f4e63360b4c05e6e85219c3673bf9a12eae04
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2018-06-14 01:45:16 +0530

    created backend server api to handle post creation

--- Backend/app.js
+++ Backend/app.js
@@ -5,20 +5,26 @@ const app = express();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((request, response, next) => {
   response.setHeader("Access-Control-Allow-Origin", "*");
-  response.setHeader("Access-Control-Allow-Header", "Origin, X-Requested-Width, Content-Type, Accept");
+  response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-Width, Content-Type, Accept");
   response.setHeader("Access-Control-Allow-Methods",
     "GET, POST, DELETE, PATCH, OPTIONS");
   next();
 });
 
 app.post('/api/posts', (request, response, next) => {
-  console.log();
+  const post = request.body;
+  console.log(post);
+  console.log(request.head);
+  response.status(201).json({
+    message: 'Post Added Successfully'
+  });
 });
 
-app.use('/api/posts', (request, response, next) => {
+app.get('/api/posts', (request, response, next) => {
   const posts = [
     {postId: 'fabjkbh455', postTitle: 'Server Post', postContent: 'Server Content'},
     {postId: 'vhv565565', postTitle: 'Server Post dummy title', postContent: 'Dummy Content'},
--- src/app/Posts/Posts.service.ts
+++ src/app/Posts/Posts.service.ts
@@ -7,11 +7,12 @@ import { HttpClient } from '@angular/common/http';
 export class PostsService {
   private posts: PostModel[] = [];
   private postsUpdated = new Subject<PostModel[]>();
+  private serverAddress = 'http://localhost:4000/api/';
 
   constructor(private HttpClientServer: HttpClient) {}
 
   getPosts() {
-    this.HttpClientServer.get<{message: string, posts: PostModel[]}>('http://localhost:4000/api/posts')
+    this.HttpClientServer.get<{message: string, posts: PostModel[]}>(this.serverAddress + 'posts')
       .subscribe((postData) => {
         this.posts = postData.posts;
         this.postsUpdated.next([...this.posts]);
@@ -19,8 +20,13 @@ export class PostsService {
   }
 
   addPost(post: PostModel) {
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    this.HttpClientServer.post<{message: string}>(this.serverAddress + 'posts', post)
+      .subscribe((responseData) => {
+        console.log(responseData);
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
+
   }
 
   getPostUpdateListener() {
